922e767d4b37ac348c50e89ceb45386663480a51
marat586l <55150069+marat586l@users.noreply.github.com>
2023-11-20 23:24:29 +0300

    Update snap.sh
    Added an option that allows  to specify the path to save the snapshot

--- snap.sh
+++ snap.sh
@@ -12,6 +12,14 @@ echo $pabkey
 ip=0
 ip=$(solana gossip | grep $pabkey | awk '{print $1}')":8899"
 echo $ip
+
+if [ -n "$2" ]
+then
+ path=$2
+else
+ path=$HOME/solana/ledger
+fi
+#echo $path
 #exit
-wget --trust-server-names -P $HOME/solana/ledger http://$ip/snapshot.tar.bz2
-wget --trust-server-names -P $HOME/solana/ledger http://$ip/incremental-snapshot.tar.bz2
+wget --trust-server-names -P "$path" http://$ip/snapshot.tar.bz2
+wget --trust-server-names -P "$path" http://$ip/incremental-snapshot.tar.bz2
